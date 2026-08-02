27215c031ddb85f64c517aba9aebd7dde0a63d11
brunvilkerby0-commits <brunvilkerby0@gmail.com>
2026-08-01 21:42:57 -0400

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,22 @@
 
+// ================================
+// 30Days - firebase.js
+// ================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCglYeFkrMIefIdNMu1J2o-kYfhCweBMlU",
+  authDomain: "days-51e9b.firebaseapp.com",
+  databaseURL: "https://days-51e9b-default-rtdb.firebaseio.com",
+  projectId: "days-51e9b",
+  storageBucket: "days-51e9b.firebasestorage.app",
+  messagingSenderId: "977450547936",
+  appId: "1:977450547936:web:d012b4bb23ddab928c2e85",
+  measurementId: "G-CYJLCGCJWB"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Services
+const auth = firebase.auth();
+const db = firebase.firestore();
